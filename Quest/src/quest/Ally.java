92dc4f6707b54a3c9fc864e216a009f9335d12b2
RobJD <robertjohnd@live.ca>
2018-01-28 19:49:09 -0500

    Added Maven

--- Quest/src/quest/Ally.java
+++
@@ -1,8 +0,0 @@
-package quest;
-
-public class Ally extends Card {
-
-    public Ally(String paramName, String paramImageFilename){
-        super(paramName, paramImageFilename);
-    }
-}
--- Quest/src/quest/Quest.java
+++
@@ -1,9 +0,0 @@
-package quest;
-
-public class Quest extends Card {
-
-    public Quest(String paramName, String paramImageFilename){
-        super(paramName, paramImageFilename);
-    }
-
-}
