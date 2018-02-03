52ce836c5e7585c00eb6d2d89639c861fa633c8a
RobJD <robertjohnd@live.ca>
2018-02-03 17:40:54 -0500

    Added log 4 j dependency, cards image on gui

--- Quest/src/main/java/quest/GameScreen.java
+++ Quest/src/main/java/quest/GameScreen.java
@@ -1,14 +1,26 @@
 package quest;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+
 
 
 public class GameScreen extends Application {
+
+    private static final Logger logger = LogManager.getLogger(GameScreen.class);
 
     public static void main(String[] args) {
         launch(args);
@@ -16,11 +28,12 @@ public class GameScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        logger.info("hi");
        initUI(primaryStage);
 
     }
 
-    public void initUI(Stage primaryStage){
+    private void initUI(Stage primaryStage){
         Pane canvas = new Pane();
         canvas.setStyle("-fx-background-color: #6F737E");
         addControlsToCanvas(canvas);
@@ -33,11 +46,34 @@ public class GameScreen extends Application {
         primaryStage.setTitle("Quest");
         primaryStage.show();
     }
-    public void addControlsToCanvas(Pane canvas){
+    private void addControlsToCanvas(Pane canvas){
 
     }
-    public void setupCardsAnimation(Pane canvas){
+    private void setupCardsAnimation(Pane canvas){
+        Image img = null;
+        File cardsDir = new File("src/main/resources/Cards/");
+        FilenameFilter imgFilter = (dir, name) -> name.toLowerCase().endsWith("jpg");
 
+        File[] cardsFile = cardsDir.listFiles(imgFilter);
+        Image[] cardsImg = new Image[cardsFile.length];
+
+        int index =0;
+        for (File cardFile : cardsFile){
+            try {
+                cardsImg[index++] = new Image(new FileInputStream(cardFile.getPath()));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ImageView imgView = new ImageView();
+        imgView.setFitHeight(250);
+        imgView.setFitWidth(250);
+        imgView.setPreserveRatio(true);
+        imgView.setImage(cardsImg[1]);
+        imgView.relocate(20,180);
+
+        canvas.getChildren().add(imgView);
     }
 
 }
