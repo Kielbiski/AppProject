da01c425a015b7526167c9781b38f8fb27db004a
Wasil <35664620+wawa316@users.noreply.github.com>
2018-02-18 15:29:59 -0500

    updated quest class

--- Quest/src/main/java/quest/Quest.java
+++ Quest/src/main/java/quest/Quest.java
@@ -3,16 +3,55 @@ package quest;
 import java.util.ArrayList;
 
 public class Quest extends Card {
-    private ArrayList<Player> players;
 
-    public Quest(String paramName, String paramImageFilename)
-    {
+    private ArrayList<Player> playerList = new ArrayList<Player>();
+    private Player hostPlayer = new Player();
+    private int numStage;
+    private int curStage;
+    private int shields;
+
+    public Quest(String paramName, String paramImageFilename, int paramNumStage, ArrayList<Player> paramPlayerList, Player paramPlayer) {
         super(paramName, paramImageFilename);
-        players = new ArrayList<>();
+        playerList = paramPlayerList;
+        hostPlayer = paramPlayer;
+        numStage = paramNumStage;
+        curStage = 0;
+        shields = numStage;
     }
 
-    void goToNextStage()
-    {
-
+    public int getShields() {
+        return shields;
     }
+
+    public int getNumStage() {
+        return numStage;
+    }
+
+    public Player getHostPlayer() {
+        return hostPlayer;
+    }
+
+    public void questPlayStageNoTest() {
+        int winnerValue = hostPlayer.getCurrentPlayPoints();
+        int i = 0;
+        int j = playerList.size();
+        while (i < j) {
+            if (winnerValue > playerList.get(i).getCurrentPlayPoints()) {
+                playerList.remove(i);
+                j--;
+            } else (winnerValue == playerList.get(i).getCurrentPlayPoints()) {
+                i++;
+            }
+        }
+        curStage++;
+    }
+
+
+    public ArrayList<Player> questWinners() {
+        for (Player player : playerList) {
+            player.setShields(player.getShields() + shields);
+        }
+        return playerList;
+    }
+
 }
