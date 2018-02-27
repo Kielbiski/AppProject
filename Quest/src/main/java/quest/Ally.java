71beae636070f561c2a85520b3bd89b7dc3b0166
Wasil <35664620+wawa316@users.noreply.github.com>
2018-02-27 13:51:41 -0500

    added log4J

--- Quest/src/main/java/quest/Ally.java
+++ Quest/src/main/java/quest/Ally.java
@@ -1,11 +1,21 @@
 package quest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Ally extends AdventureCard {
-    public Ally(String paramName, String paramImageFilename, int paramBattlePoints, int paramBids, int paramBonusBattlePoints, int paramBonusBids){
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public Ally(String paramName, String paramImageFilename, int paramBattlePoints, int paramBids, int paramBonusBattlePoints, int paramBonusBids)
+    {
+
         super(paramName, paramImageFilename);
         battlePoints = paramBattlePoints;
         bids = paramBids;
         bonusBattlePoints = paramBonusBattlePoints;
         bonusBids = paramBonusBids;
+        logger.info("Successfully called : Ally Card constructor");
+
     }
 }
