cc943fa717c7000f6cfa3137e0bc972754a6eea7
Wasil <35664620+wawa316@users.noreply.github.com>
2018-02-27 14:08:22 -0500

    Updated Card : added log4J

--- Quest/src/main/java/quest/Card.java
+++ Quest/src/main/java/quest/Card.java
@@ -1,17 +1,33 @@
 package quest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Card {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     protected String name;
     protected String imageFilename;
 
-    Card(String paramName, String paramImageFilename){
+    Card(String paramName, String paramImageFilename)
+    {
+
         name = paramName;
         imageFilename = paramImageFilename;
+        logger.info("Successfully called: Card constructor");
+
     }
+
     public String getName(){
+        logger.info("Returning Card: "+name);
         return name;
+
     }
+
     public String getImageFilename(){
+
+        logger.info("Returning Card: "+name+ " image");
         return imageFilename;
+
     }
 }
