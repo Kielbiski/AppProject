c225ac90612b3b8df3a85f898931a548c1e26dc2
Wasil <35664620+wawa316@users.noreply.github.com>
2018-03-08 12:27:28 -0500

    update to do effect

--- Quest/src/main/java/quest/DoIParticipateInQuestStrategy1.java
+++ Quest/src/main/java/quest/DoIParticipateInQuestStrategy1.java
@@ -27,36 +27,38 @@ public class DoIParticipateInQuestStrategy1 extends DoIParticipateInQuest {
         ArrayList<AdventureCard> cardPlayingWeapon = new ArrayList<>();
         ArrayList<AdventureCard> cardPlayingAllie = new ArrayList<>();
         ArrayList<AdventureCard> cardPlaying = new ArrayList<>();
-        int totalPoint= 0;
-        paramCard.sort(Comparator.comparing(AdventureCard::getBattlePoints));
+        paramCard.sort(Comparator.comparing(AdventureCard::getBattlePoints).reversed());
         for(AdventureCard card : paramCard)
         {
             if (card instanceof Ally)
             {
-                totalPoint+= card.battlePoints;
                 cardPlayingAllie.add(card);
-                if(cardPlayingAllie.size() >= 2)
-                {
-                    cardPlaying.add(cardPlayingAllie.get(0));
-                    cardPlaying.add(cardPlayingAllie.get(1));
-                    return cardPlaying;
-
-                }
-
-
             }
             if (card instanceof Weapon){
 
                 cardPlayingWeapon.add(card);
-                if(cardPlayingWeapon.size() >= 2)
-                {
-                    cardPlaying.add(cardPlayingAllie.get(0));
-                    cardPlaying.add(cardPlayingAllie.get(1));
-                    return cardPlaying;
-
-                }
-
             }
+        }
+
+
+        if(cardPlayingAllie.size() >= 1)
+        {
+            if(cardPlayingAllie.size() >=2){
+                cardPlaying.add(cardPlayingAllie.get(0));
+                cardPlaying.add(cardPlayingAllie.get(1));
+                return cardPlaying;
+            }
+            cardPlaying.add(cardPlayingAllie.get(0));
+            return cardPlaying;
+
+        }
+
+        if(cardPlayingWeapon.size() > 2)
+        {
+            cardPlaying.add(cardPlayingWeapon.get(cardPlayingWeapon.size()));
+            cardPlaying.add(cardPlayingWeapon.get(cardPlayingWeapon.size()-1));
+            return cardPlaying;
+
         }
 
         cardPlaying.clear();
@@ -64,26 +66,72 @@ public class DoIParticipateInQuestStrategy1 extends DoIParticipateInQuest {
 
     }
 
+}package quest;
 
-    public  ArrayList<AdventureCard> lastStage (ArrayList<AdventureCard> paramCard){
+import java.util.*;
+import java.util.Comparator;
 
+public class DoIParticipateInQuestStrategy1 extends DoIParticipateInQuest {
+
+    public  boolean doIParticipateInQuest(ArrayList<AdventureCard> paramCard, int paramNumStage) {
+        ArrayList<AdventureCard> weaponList = new ArrayList<>();
+        ArrayList<AdventureCard> allyList = new ArrayList<>();
+        ArrayList<AdventureCard> foeList = foeList(paramCard, 20);
+        for (AdventureCard card : paramCard) {
+            if (card instanceof Ally) {
+                allyList.add(card);
+            }
+
+            if (card instanceof Weapon) {
+                weaponList.add(card);
+            }
+        }
+        return (((allyList.size() / paramNumStage) > 2) || ((weaponList.size() / paramNumStage) > 2) || (((allyList.size() + weaponList.size()) / paramNumStage) > 2)) && foeList.size() > 2;
+    }
+
+
+    public  ArrayList<AdventureCard> playStage (ArrayList<AdventureCard> paramCard){
+
+        ArrayList<AdventureCard> cardPlayingWeapon = new ArrayList<>();
+        ArrayList<AdventureCard> cardPlayingAllie = new ArrayList<>();
         ArrayList<AdventureCard> cardPlaying = new ArrayList<>();
-        int totalPoint= 0;
-        paramCard.sort(Comparator.comparing(AdventureCard::getBattlePoints));
+        paramCard.sort(Comparator.comparing(AdventureCard::getBattlePoints).reversed());
         for(AdventureCard card : paramCard)
         {
-            if ((card instanceof Ally) ||  (card instanceof Weapon))
+            if (card instanceof Ally)
             {
-                totalPoint+= card.battlePoints;
-                cardPlaying.add(card);
-
-
+                cardPlayingAllie.add(card);
             }
+            if (card instanceof Weapon){
+
+                cardPlayingWeapon.add(card);
+            }
+        }
+
+
+        if(cardPlayingAllie.size() >= 1)
+        {
+            if(cardPlayingAllie.size() >=2){
+                cardPlaying.add(cardPlayingAllie.get(0));
+                cardPlaying.add(cardPlayingAllie.get(1));
+                return cardPlaying;
+            }
+            cardPlaying.add(cardPlayingAllie.get(0));
+            return cardPlaying;
 
         }
 
+        if(cardPlayingWeapon.size() > 2)
+        {
+            cardPlaying.add(cardPlayingWeapon.get(cardPlayingWeapon.size()));
+            cardPlaying.add(cardPlayingWeapon.get(cardPlayingWeapon.size()-1));
+            return cardPlaying;
+
+        }
+
+        cardPlaying.clear();
         return cardPlaying;
 
-
     }
+
 }
