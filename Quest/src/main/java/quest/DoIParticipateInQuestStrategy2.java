3f5bc0c4779e1a1aab60c7c7c4330f1ce4122df9
Wasil <35664620+wawa316@users.noreply.github.com>
2018-03-08 16:50:28 -0500

    Update DoIParticipateInQuestStrategy2.java

--- Quest/src/main/java/quest/DoIParticipateInQuestStrategy2.java
+++ Quest/src/main/java/quest/DoIParticipateInQuestStrategy2.java
@@ -40,24 +40,51 @@ public class DoIParticipateInQuestStrategy2 extends DoIParticipateInQuest{
     public  ArrayList<AdventureCard> playStage (ArrayList<AdventureCard> paramCard){
 
         ArrayList<AdventureCard> cardPlaying = new ArrayList<>();
+        ArrayList<AdventureCard> allyList = new ArrayList<>();
+        ArrayList<AdventureCard> weaponList = new ArrayList<>();
+        ArrayList<AdventureCard> amourList = new ArrayList<>();
         int totalPoint= 0;
         paramCard.sort(Comparator.comparing(AdventureCard::getBattlePoints));
         for(AdventureCard card : paramCard)
         {
-            if ((card instanceof Ally) ||  (card instanceof Weapon))
+            if (card instanceof Ally)
             {
-                totalPoint+= card.battlePoints;
-                cardPlaying.add(card);
-                if(totalPoint>(lastStagePoints+10))
-                {
-                    return cardPlaying;
+                if (card instanceof Amour){
+
+                    amourList.add(card);
                 }
+                else {
+                    allyList.add(card);
+                }
+            }
+            if (card instanceof Weapon){
 
-
+                weaponList.add(card);
             }
 
         }
+        if(amourList.size()!=0){
 
+            cardPlaying.add(amourList.get(0));
+            totalPoint+= amourList.get(0).getBattlePoints();
+        }
+        int counter = 0;
+        int played =0;
+        while((totalPoint<(lastStagePoints+10)) && (weaponList.size()>counter) && (allyList.size()>=1)){
+            if((cardPlaying.size() < 2) && (played==0) ){
+
+                cardPlaying.add(allyList.get(counter));
+                totalPoint+= allyList.get(counter).getBattlePoints();
+                played++;
+            }
+            else{
+                cardPlaying.add(weaponList.get(counter));
+                totalPoint+= weaponList.get(counter).getBattlePoints();
+                counter++;
+            }
+        }
+
+        lastStagePoints= totalPoint;
         return cardPlaying;
 
     }
